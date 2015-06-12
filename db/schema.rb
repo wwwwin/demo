6653c25f00afb7cd5eb14a36e89964f3d625e34a
“wwwwin” <newlife948@gmail.com>
2015-06-13 01:02:30 +0800

    db migrat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612040203) do
+ActiveRecord::Schema.define(version: 20150612131538) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "is_public"
+    t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "name"
