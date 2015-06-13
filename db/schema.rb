0235e71203fdb9ee0357b35d2a71b6e5b8613c14
“wwwwin” <newlife948@gmail.com>
2015-06-13 09:15:12 +0800

    add status to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612131538) do
+ActiveRecord::Schema.define(version: 20150613011257) do
 
   create_table "events", force: true do |t|
     t.string   "name"
