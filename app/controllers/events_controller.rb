494ae6535c2dbd88250802e7a538413f8b340b1a
“wwwwin” <newlife948@gmail.com>
2015-06-17 10:37:46 +0800

    debug formate

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,23 +2,29 @@ class EventsController < ApplicationController
 
 before_action :set_event, :only => [ :show, :edit, :update, :destroy]	
 
-	def index
-  		@events = Event.page(params[:page]).per(6)
-			
-			respond_to do |format|
-					format.html # index.html.erb
-					format.xml { render :xml => @events.to_xml }
-					format.json { render :json => @events.to_json }
-					format.atom { @feed_title = "My event list" } # index.atom.builder
-  		end
-	end
+def index
+  @events = Event.page(params[:page]).per(5)
+
+  respond_to do |format|
+    format.html # index.html.erb
+    format.xml { render :xml => @events.to_xml }
+    format.json { render :json => @events.to_json }
+    format.atom { @feed_title = "My event list" } # index.atom.builder
+  end
+end
 	
 	def new
   		@event = Event.new
 	end
 
 	def show
-	end
+  @event = Event.find(params[:id])
+  respond_to do |format|
+    format.html { @page_title = @event.name } # show.html.erb
+    format.xml # show.xml.builder
+    format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+  end
+end
 
 	def create
   		 @event = Event.new(event_params)
